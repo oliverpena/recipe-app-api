ff8b55fec85348171eab6303e5cd3a10e0511d10
oliverpena <oliverpena.itt@gmail.com>
2022-09-07 08:09:36 -0400

    Adding create superuser functionalities.

--- app/core/models.py
+++ app/core/models.py
@@ -10,9 +10,18 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email: str, password: Union[str, None] = None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address.')
         user: User = self.model(
             email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, password: Union[str, None] = None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
