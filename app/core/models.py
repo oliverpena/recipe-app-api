7c8beb46d4861db4a482f6020d8ebb1bbcedc65d
oliverpena <oliverpena.itt@gmail.com>
2022-09-22 22:30:59 -0400

    Adding user CRUD endpoints.

--- app/core/models.py
+++ app/core/models.py
@@ -30,8 +30,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active: models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
+    models.FloatField()
+
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
